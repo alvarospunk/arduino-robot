cc7435ee1e261ba7e51a61e1a69a9f17e8c71c63
Álvaro Garrido <alvarospunk@hotmail.com>
2022-10-10 22:29:12 +0200

    Parameterize values and clean unuseful comments

--- recibidor/recibidor.ino
+++ recibidor/recibidor.ino
@@ -3,21 +3,24 @@
 
 //radiofrecuencia
 #define rxPin 13
+#define serialBaudsPerSec 9600
 
 byte message[VW_MAX_MESSAGE_LEN];
 byte messageLength = VW_MAX_MESSAGE_LEN;
 size_t n = 0; //cantidad de datos recibida
+int rf_bps = 2000
+int slaveToTx = 8
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(serialBaudsPerSec);
 
   //i2c
   Wire.begin();
 
   //radiofrecuencia
-  vw_set_rx_pin(rxPin); //recibiremos por el puerto 13
-  vw_setup(2000); //instanciamos una comunicación de 2000bps
+  vw_set_rx_pin(rxPin);
+  vw_setup(rf_bps);
   vw_rx_start();
 }
 
@@ -30,9 +33,9 @@ void loop() {
     Serial.println(n);
     Serial.println(message[0]);
 
-    Wire.beginTransmission(8); // transmitimos al esclavo etiquetado como número 8
+    Wire.beginTransmission(slaveToTx);
     Wire.write(message, n);
-    Wire.endTransmission(); //cerramos la conexión
+    Wire.endTransmission();
   }
 
 
